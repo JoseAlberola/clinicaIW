b0b0de10d9e586e7c75995ba3574192afb71ddd3
ave16 <61188115+ave16@users.noreply.github.com>
2022-01-11 16:59:17 +0100

    Create datos2.sql
    Datos de prueba en la bbdd

---
+++ datos2.sql
@@ -0,0 +1,3 @@
+insert into `clinicaiw`.`usuario` (`email`, `password`, `nombre`, `tipo`, `telefono` ) VALUES ('fisio1@gmail.com' , '1234', 'fisio1', 'fisio', '902202122');
+insert into `clinicaiw`.`usuario` (`email`, `password`, `nombre`, `tipo`, `telefono` ) VALUES ('admin@gmail.com' , '1234', 'admin', 'administrador', '634447012');
+insert into `clinicaiw`.`usuario` (`email`, `password`, `nombre`, `tipo`, `telefono` ) VALUES ('recepcionista@gmail.com' , '1234', 'recepcionista', 'recepcionista', '605233197');
