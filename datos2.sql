5d2387e42eab15117c10ac7be8b5f2f095aad4e6
ave16 <61188115+ave16@users.noreply.github.com>
2022-01-11 16:59:49 +0100

    Update datos2.sql
    mas datos

--- datos2.sql
+++ datos2.sql
@@ -1,3 +1,8 @@
+INSERT INTO `clinicaiw`.`tipousuario` (`tipo`) VALUES ('administrador');
+INSERT INTO `clinicaiw`.`tipousuario` (`tipo`) VALUES ('recepcionista');
+INSERT INTO `clinicaiw`.`tipousuario` (`tipo`) VALUES ('fisio');
+INSERT INTO `clinicaiw`.`tipousuario` (`tipo`) VALUES ('usuario');
+
 insert into `clinicaiw`.`usuario` (`email`, `password`, `nombre`, `tipo`, `telefono` ) VALUES ('fisio1@gmail.com' , '1234', 'fisio1', 'fisio', '902202122');
 insert into `clinicaiw`.`usuario` (`email`, `password`, `nombre`, `tipo`, `telefono` ) VALUES ('admin@gmail.com' , '1234', 'admin', 'administrador', '634447012');
 insert into `clinicaiw`.`usuario` (`email`, `password`, `nombre`, `tipo`, `telefono` ) VALUES ('recepcionista@gmail.com' , '1234', 'recepcionista', 'recepcionista', '605233197');
