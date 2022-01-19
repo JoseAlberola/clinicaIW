11c7045f0f613b9208158f029dcf3aaabb5859f8
Pedro Lopez Mellado <pedrolome@outlook.es>
2022-01-19 13:57:51 +0100

    Arreglado error

--- clinica-cliente/src/router/index.js
+++ clinica-cliente/src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import LoginRegistro from '../components/LoginRegistro.vue'
 import DetallesFisio from '../components/DetallesFisio.vue'
-import ContenidoAdmin from '../components/ContenidoAdmin.vue'
 import ContenidoFisio from '../components/ContenidoFisio.vue'
 import Reservar from '../components/Reservar.vue'
 import Home from '../views/Home.vue'
@@ -31,11 +30,6 @@ const routes = [
       path: '/fisios/:idFisio',
       name: 'DetallesFisio',
       component: DetallesFisio
-    },
-    {
-      path: '/homeAdmin',
-      name: 'ContenidoAdmin',
-      component: ContenidoAdmin
     },
     {
       path: '/homeFisio',
