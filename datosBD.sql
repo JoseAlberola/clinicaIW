edb42a2101ac297e562b201a6c54666c32fff827
Pedro Lopez Mellado <60979312+Pedro1231232@users.noreply.github.com>
2022-01-20 18:47:04 +0100

    Delete datosBD.sql

--- datosBD.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `clinicaiw`.`tipousuario` (`tipo`) VALUES ('administrador');
-INSERT INTO `clinicaiw`.`tipousuario` (`tipo`) VALUES ('recepcionista');
-INSERT INTO `clinicaiw`.`tipousuario` (`tipo`) VALUES ('fisio');
-INSERT INTO `clinicaiw`.`tipousuario` (`tipo`) VALUES ('usuario');
